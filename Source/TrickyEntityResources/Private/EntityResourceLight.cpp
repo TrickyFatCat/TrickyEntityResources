814bc5c7ef9e884198e10843b5bbe84dd1191651
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-29 16:35:12 +0300

    Create EntityResourceLight class

---
+++ Source/TrickyEntityResources/Private/EntityResourceLight.cpp
@@ -0,0 +1,94 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "EntityResourceLight.h"
+
+UEntityResourceLight::UEntityResourceLight()
+{
+}
+
+void UEntityResourceLight::DecreaseValue(const int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return;
+	}
+	
+	ResourceData.Value -= Amount;
+	ResourceData.Value = FMath::Max(ResourceData.Value, 0);
+	// Call delegate
+
+	if (ResourceData.Value == 0)
+	{
+		// Call delegate
+	}
+}
+
+void UEntityResourceLight::IncreaseValue(const int32 Amount, bool bClampToMax)
+{
+	if (Amount <= 0)
+	{
+		return;
+	}
+	
+	ResourceData.Value += Amount;
+
+	if (bClampToMax)
+	{
+		ResourceData.Value = FMath::Min(ResourceData.Value, ResourceData.MaxValue);
+	}
+
+	// Call Delegate
+}
+
+void UEntityResourceLight::DecreaseMaxValue(int32 Amount,const bool bClampValue)
+{
+	if (Amount <= 0)
+	{
+		return;
+	}
+
+	ResourceData.MaxValue -= Amount;
+	ResourceData.MaxValue = FMath::Max(ResourceData.MaxValue, 0);
+	// Call delegate
+
+	if (bClampValue && ResourceData.Value > ResourceData.MaxValue)
+	{
+		Amount = FMath::Abs(ResourceData.MaxValue - ResourceData.Value);
+		DecreaseValue(Amount);
+	}
+}
+
+void UEntityResourceLight::IncreaseMaxValue(int32 Amount, const bool bClampValue)
+{
+	if (Amount <= 0)
+	{
+		return;
+	}
+
+	ResourceData.MaxValue += Amount;
+	// Call Delegate
+
+	if (bClampValue)
+	{
+		Amount = ResourceData.MaxValue - ResourceData.Value;
+		IncreaseValue(Amount);
+	}
+}
+
+float UEntityResourceLight::GetNormalisedValue() const
+{
+	if (ResourceData.Value == 0 || ResourceData.MaxValue == 0)
+	{
+		return 0.f;
+	}
+
+	return ResourceData.Value / ResourceData.MaxValue;
+}
+
+void UEntityResourceLight::SetResourceData(const FLightResourceData& Data)
+{
+	ResourceData = Data;
+	ResourceData.Value = ResourceData.bCustomInitialValue ? ResourceData.InitialValue : ResourceData.MaxValue;
+	// Call delegate
+}
---
+++ Source/TrickyEntityResources/Public/EntityResourceLight.h
@@ -0,0 +1,57 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "EntityResourceLight.generated.h"
+
+USTRUCT(BlueprintType)
+struct FLightResourceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, Category="EntityResourceLight")
+	int32 Value = 100;
+	
+	UPROPERTY(BlueprintReadWrite, Category="EntityResourceLight", meta=(ClampMin="0"))
+	int32 MaxValue = 100;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="EntityResourceLight", meta=(InlineEditConditionToggle))
+	bool bCustomInitialValue = false;
+
+	UPROPERTY(BlueprintReadWrite, Category="EntityResourceLight", meta=(EditCondition="bCustomInitialValue", ClampMin="0"))
+	int32 InitialValue = 100;	
+};
+
+/**
+ * 
+ */
+UCLASS(NotBlueprintable, BlueprintType)
+class TRICKYENTITYRESOURCES_API UEntityResourceLight : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UEntityResourceLight();
+
+	UFUNCTION(BlueprintCallable, Category="EntityResources|Light")
+	void DecreaseValue(const int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category="EntityResources|Light")
+	void IncreaseValue(const int32 Amount, bool bClampToMax = true);
+
+	UFUNCTION(BlueprintCallable, Category="EntityResources|Light")
+	void DecreaseMaxValue(int32 Amount, const bool bClampValue = true);
+	
+	UFUNCTION(BlueprintCallable, Category="EntityResources|Light")
+	void IncreaseMaxValue(int32 Amount, const bool bClampValue = false);
+
+	UFUNCTION(BlueprintPure, Category="EntityResources|Light")
+	float GetNormalisedValue() const;
+
+	void SetResourceData(const FLightResourceData& Data);
+private:
+	UPROPERTY(BlueprintReadOnly, Category="EntityResources|Light", meta=(AllowPrivateAccess));
+	FLightResourceData ResourceData;
+};
