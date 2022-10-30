c3f2564e9db69133535a1816eafa14b7affecdb0
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-30 19:30:08 +0200

    Create a simple resource component

---
+++ Source/TrickyEntityResources/Private/Components/SimpleResourceComponent.cpp
@@ -0,0 +1,130 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Components/SimpleResourceComponent.h"
+#include "SimpleEntityResource.h"
+#include "EntityResourcesLibrary.h"
+
+
+USimpleResourceComponent::USimpleResourceComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void USimpleResourceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ResourceObject = NewObject<USimpleEntityResource>(this);
+
+	if (ResourceObject)
+	{
+		ResourceObject->SetResourceData(ResourceData);
+		ResourceObject->OnValueDecreased.AddDynamic(this, &USimpleResourceComponent::OnValueDecreased);
+		ResourceObject->OnValueIncreased.AddDynamic(this, &USimpleResourceComponent::OnValueIncreased);
+		ResourceObject->OnValueZero.AddDynamic(this, &USimpleResourceComponent::OnValueZero);
+		ResourceObject->OnMaxValueDecreased.AddDynamic(this, &USimpleResourceComponent::OnMaxValueDecreased);
+		ResourceObject->OnMaxValueIncreased.AddDynamic(this, &USimpleResourceComponent::OnMaxValueIncreased);
+	}
+}
+
+void USimpleResourceComponent::DecreaseValue(const int32 Amount)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+	
+	ResourceObject->DecreaseValue(Amount);
+}
+
+void USimpleResourceComponent::IncreaseValue(const int32 Amount, const bool bClampToMax)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+	
+	ResourceObject->IncreaseValue(Amount, bClampToMax);
+}
+
+void USimpleResourceComponent::DecreaseMaxValue(int32 Amount, const bool bClampValue)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->DecreaseMaxValue(Amount, bClampValue);
+}
+
+void USimpleResourceComponent::IncreaseMaxValue(int32 Amount, const bool bClampValue)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->IncreaseMaxValue(Amount, bClampValue);
+}
+
+int32 USimpleResourceComponent::GetValue() const
+{
+	if (!ResourceObject)
+	{
+		return 0;
+	}
+
+	return ResourceObject->GetValue();
+}
+
+int32 USimpleResourceComponent::GetMaxValue() const
+{
+	if (!ResourceObject)
+	{
+		return 0;
+	}
+
+	return ResourceObject->GetMaxValue();
+}
+
+float USimpleResourceComponent::GetNormalisedValue() const
+{
+	if (!ResourceObject)
+	{
+		return 0.f;
+	}
+
+	return ResourceObject->GetNormalisedValue();
+}
+
+void USimpleResourceComponent::OnValueDecreased(const int32 NewValue, const int32 Amount)
+{
+	ResourceData.Value = NewValue;
+	OnResourceValueDecreased.Broadcast(NewValue, Amount);
+}
+
+void USimpleResourceComponent::OnValueIncreased(const int32 NewValue, const int32 Amount)
+{
+	ResourceData.Value = NewValue;
+	OnResourceValueIncreased.Broadcast(NewValue, Amount);
+}
+
+void USimpleResourceComponent::OnValueZero()
+{
+	OnResourceValueZero.Broadcast();
+}
+
+void USimpleResourceComponent::OnMaxValueDecreased(const int32 NewMaxValue, const int32 Amount)
+{
+	ResourceData.MaxValue = NewMaxValue;
+	OnResourceMaxValueDecreased.Broadcast(NewMaxValue, Amount);
+}
+
+void USimpleResourceComponent::OnMaxValueIncreased(const int32 NewMaxValue, const int32 Amount)
+{
+	ResourceData.MaxValue = NewMaxValue;
+	OnResourceMaxValueIncreased.Broadcast(NewMaxValue, Amount);
+}
+
---
+++ Source/TrickyEntityResources/Public/Components/SimpleResourceComponent.h
@@ -0,0 +1,81 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SimpleEntityResource.h"
+#include "Components/ActorComponent.h"
+#include "SimpleResourceComponent.generated.h"
+
+/**
+ *
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRICKYENTITYRESOURCES_API USimpleResourceComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	USimpleResourceComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|SimpleResourceComponent")
+	FOnSimpleResourceValueDecreasedSignature OnResourceValueDecreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|SimpleResourceComponent")
+	FOnSimpleResourceValueIncreasedSignature OnResourceValueIncreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|SimpleResourceComponent")
+	FOnSimpleResourceZeroValueSignature OnResourceValueZero;
+	
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|SimpleResourceComponent")
+	FOnSimpleResourceMaxValueDecreasedSignature OnResourceMaxValueDecreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|SimpleResourceComponent")
+	FOnSimpleResourceMaxValueIncreasedSignature OnResourceMaxValueIncreased;
+	
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|SimpleResourceComponent")
+	void DecreaseValue(const int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|SimpleResourceComponent")
+	void IncreaseValue(const int32 Amount, const bool bClampToMax = true);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|SimpleResourceComponent")
+	void DecreaseMaxValue(int32 Amount, const bool bClampValue = true);
+	
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|SimpleResourceComponent")
+	void IncreaseMaxValue(int32 Amount, const bool bClampValue = false);
+
+	UFUNCTION(BlueprintPure, Category="TrickyEntityResources|SimpleResourceComponent")
+	int32 GetValue() const;
+
+	UFUNCTION(BlueprintPure, Category="TrickyEntityResources|SimpleResourceComponent")
+	int32 GetMaxValue() const;
+	
+	UFUNCTION(BlueprintPure, Category="TrickyEntityResources|SimpleResourceComponent")
+	float GetNormalisedValue() const;
+private:
+	UPROPERTY()
+	USimpleEntityResource* ResourceObject = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Resource", meta=(AllowPrivateAccess))
+	FSimpleResourceData ResourceData;
+
+	UFUNCTION()
+	void OnValueDecreased(const int32 NewValue, const int32 Amount);
+	
+	UFUNCTION()
+	void OnValueIncreased(const int32 NewValue, const int32 Amount);
+
+	UFUNCTION()
+	void OnValueZero();
+
+	UFUNCTION()
+	void OnMaxValueDecreased(const int32 NewMaxValue, const int32 Amount);
+
+	UFUNCTION()
+	void OnMaxValueIncreased(const int32 NewMaxValue, const int32 Amount);
+};
