908670f00cdddc53cc1c665bdb4058d0d317815a
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-30 20:43:10 +0200

    Create a resource component

---
+++ Source/TrickyEntityResources/Private/Components/ResourceComponent.cpp
@@ -0,0 +1,267 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Components/ResourceComponent.h"
+
+
+UResourceComponent::UResourceComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UResourceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ResourceObject = NewObject<UEntityResource>(this);
+
+	if (ResourceObject)
+	{
+		ResourceObject->SetResourceData(ResourceData);
+		ResourceObject->SetAutoDecreaseData(AutoDecreaseData);
+		ResourceObject->SetAutoIncreaseData(AutoIncreaseData);
+		ResourceObject->OnValueDecreased.AddDynamic(this, &UResourceComponent::OnValueDecreased);
+		ResourceObject->OnValueIncreased.AddDynamic(this, &UResourceComponent::OnValueIncreased);
+		ResourceObject->OnValueZero.AddDynamic(this, &UResourceComponent::OnValueZero);
+		ResourceObject->OnMaxValueDecreased.AddDynamic(this, &UResourceComponent::OnMaxValueDecreased);
+		ResourceObject->OnMaxValueIncreased.AddDynamic(this, &UResourceComponent::OnMaxValueIncreased);
+		ResourceObject->OnAutoDecreaseStarted.AddDynamic(this, &UResourceComponent::OnAutoDecreaseStarted);
+		ResourceObject->OnAutoIncreaseStarted.AddDynamic(this, &UResourceComponent::OnAutoIncreaseStarted);
+		ResourceObject->OnAutoDecreaseStopped.AddDynamic(this, &UResourceComponent::OnAutoDecreaseStopped);
+		ResourceObject->OnAutoIncreaseStopped.AddDynamic(this, &UResourceComponent::OnAutoIncreaseStopped);
+	}
+}
+
+void UResourceComponent::TickComponent(float DeltaTime,
+                                       ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UResourceComponent::DecreaseValue(const float Amount) const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->DecreaseValue(Amount);
+}
+
+void UResourceComponent::IncreaseValue(const float Amount, const bool bClampToMax) const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->IncreaseValue(Amount, bClampToMax);
+}
+
+void UResourceComponent::DecreaseMaxValue(const float Amount, const bool bClampValue) const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->DecreaseMaxValue(Amount, bClampValue);
+}
+
+void UResourceComponent::IncreaseMaxValue(const float Amount, const bool bClampValue) const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->IncreaseMaxValue(Amount, bClampValue);
+}
+
+float UResourceComponent::GetValue() const
+{
+	if (!ResourceObject)
+	{
+		return -1.f;
+	}
+
+	return ResourceObject->GetValue();
+}
+
+float UResourceComponent::GetMaxValue() const
+{
+	if (!ResourceObject)
+	{
+		return -1.f;
+	}
+
+	return ResourceObject->GetMaxValue();
+}
+
+float UResourceComponent::GetNormalisedValue() const
+{
+	if (!ResourceObject)
+	{
+		return -1.f;
+	}
+
+	return ResourceObject->GetNormalisedValue();
+}
+
+void UResourceComponent::SetAutoDecreaseEnabled(const bool bIsEnabled)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	AutoDecreaseData.bIsEnabled = bIsEnabled;
+	ResourceObject->SetAutoDecreaseEnabled(bIsEnabled);
+}
+
+void UResourceComponent::SetAutoIncreaseEnabled(const bool bIsEnabled)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	AutoIncreaseData.bIsEnabled = bIsEnabled;
+	ResourceObject->SetAutoIncreaseEnabled(bIsEnabled);
+}
+
+FResourceAutoData UResourceComponent::GetAutoDecreaseData() const
+{
+	FResourceAutoData Data = AutoDecreaseData;
+
+	if (ResourceObject)
+	{
+		ResourceObject->GetAutoDecreaseData(Data);
+	}
+
+	return Data;
+}
+
+FResourceAutoData UResourceComponent::GetAutoIncreaseData() const
+{
+	FResourceAutoData Data = AutoIncreaseData;
+
+	if (ResourceObject)
+	{
+		ResourceObject->GetAutoIncreaseData(Data);
+	}
+
+	return Data;
+}
+
+void UResourceComponent::SetAutoDecreaseData(const FResourceAutoData& Data)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	AutoDecreaseData = Data;
+	ResourceObject->SetAutoDecreaseData(AutoDecreaseData);
+}
+
+void UResourceComponent::SetAutoIncreaseData(const FResourceAutoData& Data)
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	AutoIncreaseData = Data;
+	ResourceObject->SetAutoDecreaseData(AutoIncreaseData);
+}
+
+void UResourceComponent::StartAutoDecrease() const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->StartAutoDecrease();
+}
+
+void UResourceComponent::StartAutoIncrease() const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->StartAutoIncrease();
+}
+
+void UResourceComponent::StopAutoDecrease() const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->StopAutoDecrease();
+}
+
+void UResourceComponent::StopAutoIncrease() const
+{
+	if (!ResourceObject)
+	{
+		return;
+	}
+
+	ResourceObject->StopAutoIncrease();
+}
+
+void UResourceComponent::OnValueDecreased(const float NewValue, const float Amount)
+{
+	ResourceData.Value = NewValue;
+	OnResourceValueDecreased.Broadcast(NewValue, Amount);
+}
+
+void UResourceComponent::OnValueIncreased(const float NewValue, const float Amount)
+{
+	ResourceData.Value = NewValue;
+	OnResourceValueIncreased.Broadcast(NewValue, Amount);
+}
+
+void UResourceComponent::OnValueZero()
+{
+	OnResourceValueZero.Broadcast();
+}
+
+void UResourceComponent::OnMaxValueDecreased(const float NewValue, const float Amount)
+{
+	ResourceData.MaxValue = NewValue;
+	OnResourceMaxValueDecreased.Broadcast(NewValue, Amount);
+}
+
+void UResourceComponent::OnMaxValueIncreased(const float NewValue, const float Amount)
+{
+	ResourceData.MaxValue = NewValue;
+	OnResourceMaxValueIncreased.Broadcast(NewValue, Amount);
+}
+
+void UResourceComponent::OnAutoDecreaseStarted()
+{
+	OnResourceAutoDecreaseStarted.Broadcast();
+}
+
+void UResourceComponent::OnAutoIncreaseStarted()
+{
+	OnResourceAutoIncreaseStarted.Broadcast();
+}
+
+void UResourceComponent::OnAutoDecreaseStopped()
+{
+	OnResourceAutoDecreaseStopped.Broadcast();
+}
+
+void UResourceComponent::OnAutoIncreaseStopped()
+{
+	OnResourceAutoIncreaseStopped.Broadcast();
+}
---
+++ Source/TrickyEntityResources/Public/Components/ResourceComponent.h
@@ -0,0 +1,151 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EntityResource.h"
+#include "Components/ActorComponent.h"
+#include "ResourceComponent.generated.h"
+
+
+UCLASS(ClassGroup=(TrickyEntityResources), meta=(BlueprintSpawnableComponent))
+class TRICKYENTITYRESOURCES_API UResourceComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UResourceComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime,
+	                           ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceValueDecreasedSignature OnResourceValueDecreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceValueIncreasedSignature OnResourceValueIncreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceValueZeroSignature OnResourceValueZero;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceMaxValueDecreasedSignature OnResourceMaxValueDecreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceMaxValueIncreasedSignature OnResourceMaxValueIncreased;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceAutoDecreaseStartedSignature OnResourceAutoDecreaseStarted;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceAutoIncreaseStartedSignature OnResourceAutoIncreaseStarted;
+	
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceAutoDecreaseStoppedSignature OnResourceAutoDecreaseStopped;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyEntityResources|ResourceComponent")
+	FOnResourceAutoIncreaseStoppedSignature OnResourceAutoIncreaseStopped;
+	
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void DecreaseValue(const float Amount) const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void IncreaseValue(const float Amount, const bool bClampToMax = true) const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void DecreaseMaxValue(float Amount, const bool bClampValue = true) const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void IncreaseMaxValue(float Amount, const bool bClampValue = false) const;
+
+	UFUNCTION(BlueprintPure, Category="TrickyEntityResources|ResourceComponent")
+	float GetValue() const;
+
+	UFUNCTION(BlueprintPure, Category="TrickyEntityResources|ResourceComponent")
+	float GetMaxValue() const;
+
+	UFUNCTION(BlueprintPure, Category="TrickyEntityResources|ResourceComponent")
+	float GetNormalisedValue() const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void SetAutoDecreaseEnabled(const bool bIsEnabled);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void SetAutoIncreaseEnabled(const bool bIsEnabled);
+
+	UFUNCTION(BlueprintGetter, Category="TrickyEntityResources|ResourceComponent")
+	FResourceAutoData GetAutoDecreaseData() const;
+
+	UFUNCTION(BlueprintGetter, Category="TrickyEntityResources|ResourceComponent")
+	FResourceAutoData GetAutoIncreaseData() const;
+
+	UFUNCTION(BlueprintSetter, Category="TrickyEntityResources|ResourceComponent")
+	void SetAutoDecreaseData(const FResourceAutoData& Data);
+
+	UFUNCTION(BlueprintSetter, Category="TrickyEntityResources|ResourceComponent")
+	void SetAutoIncreaseData(const FResourceAutoData& Data);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void StartAutoDecrease() const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void StartAutoIncrease() const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void StopAutoDecrease() const;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEntityResources|ResourceComponent")
+	void StopAutoIncrease() const;
+private:
+	UPROPERTY()
+	UEntityResource* ResourceObject = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Resource", meta=(AllowPrivateAccess))
+	FResourceData ResourceData;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetAutoDecreaseData,
+		BlueprintSetter=SetAutoDecreaseData,
+		Category="Resource",
+		meta=(AllowPrivateAccess))
+	FResourceAutoData AutoDecreaseData;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetAutoIncreaseData,
+		BlueprintSetter=SetAutoIncreaseData,
+		Category="Resource",
+		meta=(AllowPrivateAccess))
+	FResourceAutoData AutoIncreaseData;
+
+	UFUNCTION()
+	void OnValueDecreased(const float NewValue, const float Amount);
+
+	UFUNCTION()
+	void OnValueIncreased(const float NewValue, const float Amount);
+
+	UFUNCTION()
+	void OnValueZero();
+
+	UFUNCTION()
+	void OnMaxValueDecreased(const float NewValue, const float Amount);
+
+	UFUNCTION()
+	void OnMaxValueIncreased(const float NewValue, const float Amount);
+
+	UFUNCTION()
+	void OnAutoDecreaseStarted();
+
+	UFUNCTION()
+	void OnAutoIncreaseStarted();
+
+	UFUNCTION()
+	void OnAutoDecreaseStopped();
+
+	UFUNCTION()
+	void OnAutoIncreaseStopped();
+};
